ba45a4861b646e2eb4b870074af66eb2ac59f87d
Numan Ali <numan4599322ali@gmail.com>
2026-02-13 12:33:38 +0530

    ff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
 
-
 const poppinsfont = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
@@ -21,16 +20,12 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-
   return (
-    <ClerkProvider>  
+    <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${poppinsfont.className} antialiased`}
-        >
-            <Navbar/>
-            {children}
+        <body className={`${poppinsfont.className} antialiased`}>
+          <Navbar />
+          {children}
         </body>
       </html>
     </ClerkProvider>
